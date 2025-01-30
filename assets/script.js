397da813d1056c99db65ff9ddd3c778bd6d1106c
Nicolas Gottiniaux <nicolas.gottiniaux@gmail.com>
2025-01-30 17:06:16 +0100

    adding dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,29 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+createDots();
+const numberOfSlide = slides.length;
+
+
+const arrowLeft = document.querySelector('.arrow_left');
+arrowLeft.addEventListener("click", () => {
+    console.log("Vous avez cliqué sur la flèche gauche")
+});
+
+const arrowRight = document.querySelector('.arrow_right');
+arrowRight.addEventListener("click", () => {
+    console.log("Vous avez cliqué sur la flèche droite")
+});
+
+
+function createDots(){
+	const dots = document.querySelector(".dots");	
+   for (let index = 0; index < slides.length; index++) {
+	   const dot= document.createElement("div");
+	   dot.setAttribute("class", "dot");
+	   dots.appendChild(dot);
+   } 
+}
+
+
